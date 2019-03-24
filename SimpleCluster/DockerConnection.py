279a899b5cd9559d8bd454f334afd2de00965be4
faizan khan <faizan10114@gmail.com>
2019-03-24 04:13:30 -0400

    basic working cli with etcd connected

--- SimpleCluster/DockerConnection.py
+++ SimpleCluster/DockerConnection.py
@@ -1,4 +1,6 @@
 import docker
+import argparse
+
 from StateStorage import *
 CMD="nc -e /bin/cat -l -p 8200"
 
@@ -8,12 +10,67 @@ HOST_PORT= 8100
 DOC_PORT = '8200/tcp'
 
 
-
-
-
 if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Specify your app name and command')
+    parser.add_argument('-n', '--name', help='name of the app', required=True)
+    parser.add_argument('-c', '--command', help='scale-up, scale-down, start, list', required=True)
+    args = vars(parser.parse_args())
+
     client = docker.from_env()
-    # client.containers.run(IMAGE, CMD, name="test2", ports={DOC_PORT:HOST_PORT}, detach=True)
+
+    if args['command']=='start':
+        #you start the thing
+        container = client.containers.run(IMAGE, CMD, ports={DOC_PORT:HOST_PORT}, detach=True)
+        nameOfApp = args['name']
+        storeContainer(nameOfApp, container.id)
+
+    elif args['command']=='scale-up':
+        #you scale the thing
+        print("sinide this")
+
+    elif args['command']=='scale-down':
+        # you scale the thing
+
+        #you scale the thing
+        print("sinide this")
+
+        # nameOfApp = args['name']
+        #
+        # client.containers.get(nameOfApp)
+        # print("sinide this")
+
+    elif args['command']=='list':
+        # nameOfApp = args['name']
+        # children = getContainers(nameOfApp)
+        # print(list(map( lambda x: x.value, children)))
+
+        containers = client.containers.list(all)
+
+        print(list(map(lambda x: x.name+", "+x.status, containers)))
+
+
+
+
+
+    # children = getContainers('abc')
+    # print(list(map( lambda x: x.value, children)))
+    #
+    # client = docker.from_env()
+    #
+
+    #
+    #
+# )
+
+    #
+    # parser.add_argument("")
+    # client = docker.from_env()
+    #
+    #
+    #
+    # print(writeToFile(data, 'storage.json'))
+    #
+    # print(readFromFile('storage.json'))
 
     # containers = client.containers.list(all)
     #
@@ -33,7 +90,5 @@ if __name__=="__main__":
     # })
 
 
-    print(writeToFile(data, 'storage.json'))
 
-    print(readFromFile('storage.json'))
 
--- SimpleCluster/StateStorage.py
+++ SimpleCluster/StateStorage.py
@@ -1,5 +1,6 @@
 import json
 import os
+import etcd
 
 def readFromFile(fileName):
     with open(fileName, 'r') as infile:
@@ -23,7 +24,27 @@ def writeToFile(data, fileName):
             json.dump(data, f)
 
 
+client = None
 
+def _initializeEtcd():
+    global client
+    client = etcd.Client(host='127.0.0.1', port=2379)
+    return client
+
+def storeContainer(appName, containerID):
+    client = _initializeEtcd()
+    client.write('/app/'+appName, containerID, append=True)
+
+def getContainers(appName):
+    client = _initializeEtcd()
+    dir = client.get('/app/'+appName)
+    return dir.children
 
 if __name__=="__main__":
     print("something")
+
+    #     client = storeContainer('abc', '1234')
+    #
+    #
+    #     children = getContainers('abc')
+    #     print(list(map( lambda x: x.value, children)))
