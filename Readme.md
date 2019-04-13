d95c81a1ecbf040b6e323e099a9be9c654baa7c0
faizan khan <faizan10114@gmail.com>
2019-04-12 23:54:43 -0400

    update readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,11 @@
-simple cloud/cluster management thing
+###Steps
+1. do pip install -r requirements
+2. run python SimpleCluster/Manager.py
+
+
+###Commands
+3. start app-name, will start a new app
+4. list app-name will list the containers for that app
+5. stop app-name, will stop the app
+6. scaleup app-name number-of-workers, will scale the app to the specified number
+7. scaledown app-name number-of-workers, will scale down the app to the specified number
--- SimpleCluster/AutoScaling.py
+++ SimpleCluster/AutoScaling.py
@@ -1,277 +1,87 @@
-import sys
 import docker
-import logging
-import socket
-import os
-import shutil
 
-from SimpleCluster.NginxConfigBuilder import *
 from SimpleCluster.StateStorage import *
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)s %(threadName)s %(levelname)s %(message)s')
-
-CONFIG_DIR = 'loadbalancer/configs/'
-
-LB = "loadbalancer"
-SERVERS = "servers"
-ADDR = "address"
-
-IMAGE_APP = "hasnainmamdani/comp598_proj_testapp"
-IMAGE_LB = "hasnainmamdani/comp598_proj_loadbalancer"
+from SimpleCluster.Manager import *
 
 
+def calculate_cpu_percent(d, previous_cpu, previous_system):
+    # import json
+    # du = json.dumps(d, indent=2)
+    # logger.debug("XXX: %s", du)
+    cpu_percent = 0.0
+    cpu_total = float(d["cpu_stats"]["cpu_usage"]["total_usage"])
+    cpu_delta = cpu_total - previous_cpu
+    cpu_system = float(d["cpu_stats"]["system_cpu_usage"])
+    system_delta = cpu_system - previous_system
+    online_cpus = d["cpu_stats"].get("online_cpus", len(d["cpu_stats"]["cpu_usage"]["percpu_usage"]))
+    if system_delta > 0.0:
+        cpu_percent = (cpu_delta / system_delta) * online_cpus * 100.0
+    #print(cpu_percent, cpu_system, cpu_total)
+    return cpu_percent, cpu_total, cpu_system
 
-def get_free_port():
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.bind(('localhost', 0))
-  addr, port = s.getsockname()
-  s.close()
-  return port
+def get_cpu_percent(container_id):
+    global container_state, client
 
+    container = client.containers.get(container_id)
+    previous_cpu= container_state[container_id][0]
+    previous_system= container_state[container_id][1]
 
-# def rebuild_existing_state():
+    currenStats = container.stats(decode=True)
+    d = next(currenStats)
+    cpu_percent, current_cpu, current_system = calculate_cpu_percent(d, previous_cpu, previous_system)
 
-
-def scale_up(app_name, num_workers):
-    client = docker.from_env()
-    running_containers = getWorkersForApp(app_name)
-    if not running_containers:
-        logger.error("App % is not active" % app_name)
-
-    for i in range(num_workers):
-        container_app = client.containers.run(IMAGE_APP, "python app.py", stderr=True, stdin_open=True, remove=True,
-                                              detach=True)
-        container_app_ip_addr = client.containers.get(container_app.id).attrs['NetworkSettings']['Networks']['bridge'][
-            'IPAddress']
-        add_server(app_name, container_app_ip_addr)
-
-        saveAppState(app_name, container_app.id)
-    container_lb = client.containers.get(app_name + "-loadbalancer")
-    container_lb.exec_run('nginx -s reload')
-    saveLbState(app_name, container_lb.id)
-
-    logger.info("Added %d more workers to the application %s. Total workers now = %d" % (
-    num_workers, app_name, len(running_containers) + num_workers))
+    container_state[worker]=[current_cpu, current_system]
+    return cpu_percent
 
 
+def start_auto_scaling(app_name):
+
+    container_state = {}
+    for worker in getWorkersForApp(app_name):
+        container_state[worker]= [0,0]
+
+    while(True):
+        for worker in getWorkersForApp(app_name):
+            if get_cpu_percent(worker) >= 0.25:
+                scale_up(app_name, 1)
 
 if __name__=="__main__":
-    # container_state_file = open("../containerState.p", "rb")
-
-    client = docker.from_env()
-
-    active_apps={}
-
-    # try:
-    #     active_apps= pickle.load(container_state_file)
-    # except EOFError:
-    #     # file does not exist create one
-    #     active_apps={}
-    #     container_state_file = open("../containerState.p", "wb+")
-    #     pickle.dump(active_apps, container_state_file)
-
-
-    # localhost for now
-    ip_addr = socket.gethostbyname('localhost')
-
-    # shutil.rmtree(CONFIG_DIR, ignore_errors=True)
-    # os.mkdir(CONFIG_DIR)
-
-    while True:
-
-        command = input("Next command: ").split(' ')
-
-        if command[0] == 'start':
-
-            if len(command) != 2:
-                logger.info("Incorrect format. Enter \"start <app_name>\"")
-                continue
-
-            app_name = command[1]
-
-            running_containers = getWorkersForApp(app_name)
-
-            if running_containers:
-                logger.info("App %s already running" % app_name)
-
-            else:
-                container_app = client.containers.run(IMAGE_APP, "python app.py", stderr=True, stdin_open=True, remove=True, detach=True)
-                saveAppState(app_name, container_app.id)
-
-
-                container_app_ip_addr = client.containers.get(container_app.id).attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-                port = get_free_port()
-                os.mkdir(CONFIG_DIR+app_name)
-                create_nginx_config(port, app_name, container_app_ip_addr)
-                container_lb = client.containers.run(IMAGE_LB, tty=True, stderr=True, stdin_open=True, ports={str(port)+'/tcp': port},
-                                                   name=app_name+"-loadbalancer", remove=True, detach=True,
-                                                   volumes={os.getcwd()+'/'+CONFIG_DIR+app_name: {'bind': '/etc/nginx', 'mode': 'ro'}})
-                container_lb.exec_run('nginx -s reload')
-
-                saveLbState(app_name, container_lb.id)
-
-                logger.info("Application %s started with one worker at %s:%d" % (app_name, ip_addr, port))
-
-        elif command[0] == 'stop':
-            if len(command) != 2:
-                logger.info("Incorrect format. Enter \"stop <app_name>\"")
-                continue
-
-            app_name = command[1]
-
-            running_containers = getWorkersForApp(app_name)
-            if not running_containers:
-                logger.info("Application %s is not active" % app_name)
-                continue
-
-            for container_id in running_containers:
-                try:
-                    client.containers.get(container_id).stop(timeout=0)
-                except:
-                    pass
-
-            load_balancer_id = getLbForApp(app_name)
-
-            if load_balancer_id:
-                try:
-                    client.containers.get(load_balancer_id).stop(timeout=0)
-                except:
-                    pass
-
-            deleteLbState(app_name)
-            deleteAppState(app_name)
-
-
-            #TODO: add this again when fault tolerance is added
-            shutil.rmtree(CONFIG_DIR+app_name)
-
-            logger.info("Application %s stopped. All relevant containers destroyed" % (app_name))
-
-        elif command[0] == 'scaleup':
-
-            if len(command) != 3:
-                logger.error("Incorrect format. Enter \"scaleup <app_name> <num_workers>\"")
-                continue
-
-            app_name = command[1]
-            num_workers = int(command[2])
-
-            running_containers = getWorkersForApp(app_name)
-            if not running_containers:
-                logger.error("App % is not active" % app_name)
-                continue
-
-            if num_workers < 1:
-                logger.error("Invalid number of worker to add = %d. It should be greater than zero. Skipping." %(num_workers))
-                continue
-
-            for i in range(num_workers):
-                container_app = client.containers.run(IMAGE_APP, "python app.py", stderr=True, stdin_open=True, remove=True, detach=True)
-                container_app_ip_addr = client.containers.get(container_app.id).attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-                add_server(app_name, container_app_ip_addr)
-
-                saveAppState(app_name, container_app.id)
-            container_lb = client.containers.get(app_name+"-loadbalancer")
-            container_lb.exec_run('nginx -s reload')
-            saveLbState(app_name, container_lb.id)
-
-            logger.info("Added %d more workers to the application %s. Total workers now = %d" % (num_workers, app_name, len(running_containers)+num_workers))
-
-        elif command[0] == 'scaledown':
-
-            if len(command) != 3:
-                logger.error("Incorrect format. Enter \"scaledown <app_name> <num_workers>\"")
-                continue
-
-            app_name = command[1]
-            num_workers = int(command[2])
-
-            running_containers = getWorkersForApp(app_name)
-            if not running_containers:
-                logger.error("Application % is not active" % app_name)
-                continue
-
-            if num_workers < 1:
-                logger.error("Invalid number of worker to remove = %d. It should be greater than zero. Skipping." %(num_workers))
-                continue
-
-            if len(running_containers) <= num_workers:
-                logger.error("User intended to remove all workers. Consider stopping the application with the 'stop' command.")
-                continue
-
-            for i in range(num_workers):
-                container_id= running_containers.pop()
-                container_app_ip_addr = client.containers.get(container_id).attrs['NetworkSettings']['Networks']['bridge']['IPAddress']
-                client.containers.get(container_id).stop(timeout=0)
-                remove_server(app_name, container_app_ip_addr)
-
-                deleteWorkerforApp(app_name, container_id)
-
-            container_lb = client.containers.get(app_name+"-loadbalancer")
-            container_lb.exec_run('nginx -s reload')
-
-            logger.info("Removed %d workers from the application %s. Total workers now = %d" % (num_workers, app_name, len(running_containers)-num_workers))
-
-        elif command[0] == 'list':
-            if len(command) != 2:
-                logger.info("Incorrect format. Enter \"list <app_name>\"")
-                continue
-
-            app_name = command[1]
-
-            runningWorkerIds = getWorkersForApp(app_name)
-            if not runningWorkerIds:
-                print("{0} application is not running".format(app_name))
-            # stop all workers and the load balancer
-            else:
-                print("{} workers running for {}".format(len(runningWorkerIds), app_name))
-                print("Container Ids")
-                for worker in runningWorkerIds:
-                    print(worker)
-
-
-        elif command[0] == 'exit':
-            sys.exit(0)
-
-        elif command[0] == 'ps':
-            total_apps = getTotalApps()
-            if len(total_apps)==0:
-                print("{0} applications running".format(0))
-            # stop all workers and the load balancer
-            else:
-                for app in total_apps:
-                    print(app)
-
-        elif command[0] == 'startautoscale':
-            if len(command) != 2:
-                logger.info("Incorrect format. Enter \"startautoscale <app_name>\"")
-                continue
-
-            app_name = command[1]
-
-            #TODO: implement autoscale
-            # runningWorkerIds = getWorkersForApp(app_name)
-            # if not runningWorkerIds:
-            #     print("{0} application is not running".format(app_name))
-            #
-            # else:
-            #     # start the auto scaler thread
-            #     t = threading.Thread(name='auto_scaling', target=start_auto_scaling, daemon=True, args=(applicationName,))
-            #     t.start()
-
-        elif command[0] == 'stopautoscale':
-            if len(command) != 2:
-                logger.info("Incorrect format. Enter \"stopautoscale <app_name>\"")
-                continue
-
-            app_name = command[1]
-
-            #TODO: implement unsubscribe
-            # runningWorkerIds = getWorkersForApp(app_name)
-            # t.start()
+    app_name='testApp1'
+    # client = docker.from_env()
 
 
 
-        else:
-            logger.error("Invalid command")
+
+
+
+
+
+    # parser = argparse.ArgumentParser(description='Specify your app name and command')
+    # parser.add_argument('-n', '--name', help='name of the app', required=True)
+    # parser.add_argument('-c', '--command', help='scale-up, scale-down, start, list', required=True)
+    # args = vars(parser.parse_args())
+    #
+    # client = docker.from_env()
+    #
+    # if args['command']=='start':
+    #     #you start the thing
+    #     container = client.containers.run(IMAGE, CMD, ports={DOC_PORT:HOST_PORT}, detach=True)
+    #     nameOfApp = args['name']
+    #     storeContainer(nameOfApp, container.id)
+    #
+    # elif args['command']=='scale-up':
+    #     #you scale the thing
+    #     print("sinide this")
+    #
+    # elif args['command']=='scale-down':
+    #     # you scale the thing
+    #
+    #     #you scale the thing
+    #     print("sinide this")
+    #
+    #
+    # elif args['command']=='list':
+    #
+    #     containers = client.containers.list(all)
+    #
+    #     print(list(map(lambda x: x.name+", "+x.status, containers)))
